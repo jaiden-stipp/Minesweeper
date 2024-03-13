1d2a0aadb9da7ac1f2d53295a46af47576a1659f
parjaap <162784448+parjaap@users.noreply.github.com>
2024-03-12 21:22:53 -0700

    Add files via upload

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,24 +1,72 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
-import javax.swing.*;
+import java.util.Scanner;
 
 public class Minesweeper {
-    
-    int tileSize = 70;
-    int numRows = 8;
-    int numCols = numRows;
-    int boardWidth = numCols * tileSize; 
-    int boardHeight = numRows * tileSize;
+    private final int[][] board;
+    private final boolean[][] revealed;
+    private final int numRows;
+    private final int numCols;
+    private final int numMines;
 
-    JFrame frame = new JFrame("Minesweeper");
-
-    Minesweeper() {
-        frame.setVisible(true);
-        frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
+    public Minesweeper(int numRows, int numCols, int numMines) {
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.numMines = numMines;
+        this.board = new int[numRows][numCols];
+        this.revealed = new boolean[numRows][numCols];
+        initializeBoard();
+        placeMines();
+        calculateNumbers();
     }
-}
+
+    private void initializeBoard() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                board[i][j] = 0;
+                revealed[i][j] = false;
+            }
+        }
+    }
+
+    private void placeMines() {
+        int minesPlaced = 0;
+        while (minesPlaced < numMines) {
+            int row = (int) (Math.random() * numRows);
+            int col = (int) (Math.random() * numCols);
+            if (board[row][col] != -1) {
+                board[row][col] = -1;
+                minesPlaced++;
+            }
+        }
+    }
+
+    private void calculateNumbers() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (board[i][j] != -1) {
+                    int count = 0;
+                    for (int ii = Math.max(0, i - 1); ii <= Math.min(numRows - 1, i + 1); ii++) {
+                        for (int jj = Math.max(0, j - 1); jj <= Math.min(numCols - 1, j + 1); jj++) {
+                            if (board[ii][jj] == -1) {
+                                count++;
+                            }
+                        }
+                    }
+                    board[i][j] = count;
+                }
+            }
+        }
+    }
+
+    public void printBoard(boolean showMines) {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (revealed[i][j] || showMines && board[i][j] == -1) {
+                    System.out.print(board[i][j] == -1 ? "* " : board[i][j] + " ");
+                } else {
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
